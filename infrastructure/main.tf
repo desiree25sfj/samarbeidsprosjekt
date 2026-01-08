dc6b91e3e2f2087e364b8e5e6c7cdf796b133627
zabronax <zabronax@gmail.com>
2026-01-08 14:43:14 +0100

    define an example deployment

---
+++ infrastructure/main.tf
@@ -0,0 +1,25 @@
+resource "hcloud_ssh_key" "member_1_key" {
+  name       = "member-1-ssh-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQ4V7XaYqLwEydVYkWyYXhMHpqvEj8ifzT8fEgycpCP student-1"
+}
+
+resource "hcloud_ssh_key" "member_2_key" {
+  name       = "member-2-ssh-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIECHbdhK3FrUIuL1Pa8zo2OzwB+mvU/Er0K4W3OgbamX student-2"
+}
+
+resource "hcloud_server" "server" {
+  name        = "example-server"
+  server_type = "cax31"
+  image       = "debian-13"
+
+  ssh_keys = [
+    hcloud_ssh_key.member_1_key.id,
+    hcloud_ssh_key.member_2_key.id
+  ]
+}
+
+output "server_ip" {
+  value     = hcloud_server.server.ipv4_address
+  sensitive = false
+}
